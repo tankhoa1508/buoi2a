3b564900d2059575cd09a5d7bced3af5726feaa3
tankhoa1508 <khoanguyen150820@gmail.com>
2024-07-22 10:14:55 +0700

    update lan 2

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
